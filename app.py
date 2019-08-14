a6367764f84d32b29fd8b687d56354c290619b10
ayk-oishi <53889830+ayk-oishi@users.noreply.github.com>
2019-08-14 18:51:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ import dash_html_components as html
 import plotly.graph_objs as go
 
 ########### Define your variables ######
-myheading = "What's your favorite Dunkin' Donut?"
-mytitle = "Top 3 Flavors"
-mylabels = ['Chocolate Frosted', 'French Cruller', 'Boston Creme']
-myvalues = [45,25,7]
+myheading = "The most popular drink at Starbucks"
+mytitle = "Top 3 Drinks"
+mylabels = ['Drip Coffee', 'Double Chocolaty Chip Crème Frappuccino', 'Chai tea latte']
+myvalues = [30,20,7]
 color1 = 'e11383'
 color2 = 'f5821f'
 color3 = '683817'
-tabtitle = 'dunkin'
+tabtitle = 'starbucks'
 sourceurl = 'https://brandpalettes.com/dunkin-donuts-color-codes/'
 githublink = 'https://github.com/austinlasseter/dash-piechart-example'
 
